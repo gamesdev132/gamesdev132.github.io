48c0e8face0d0af6a21180110ac61484de350c64
MIGUELEZ MATTEO p2101471 <matteo.miguelez@etu.univ-lyon1.fr>
2024-10-10 19:54:29 +0200

    style(trio-latest): add basic styling

--- src/app/latest-scores/trio/trio.component.ts
+++ src/app/latest-scores/trio/trio.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input } from '@angular/core';
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { faCalendarDays, faCrown, faPeopleGroup } from '@fortawesome/free-solid-svg-icons';
 import { Trio } from "app/@shared/interface/trio";
 import { CardModule } from "primeng/card";
 import { CarouselModule } from "primeng/carousel";
@@ -8,13 +10,17 @@ import { CarouselModule } from "primeng/carousel";
   standalone: true,
   imports: [
     CarouselModule,
-    CardModule
+    CardModule,
+    FontAwesomeModule
   ],
   templateUrl: './trio.component.html',
   styleUrl: './trio.component.css'
 })
 export class TrioComponent {
   @Input({required: true}) scores: Trio[] = [];
+  faCrown = faCrown;
+  faGroup = faPeopleGroup
+  faDate = faCalendarDays
 
   getPlayerList(players: string[]): string {
     return players.join(', ')
