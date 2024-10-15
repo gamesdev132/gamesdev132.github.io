200716a7d9b58cef79fb2329a97d09a873b6c108
MIGUELEZ MATTEO p2101471 <matteo.miguelez@etu.univ-lyon1.fr>
2024-10-15 22:07:21 +0200

    feat(scores): adapt point games scores

--- src/app/latest-scores/point-games/point-games.component.ts
+++ src/app/latest-scores/point-games/point-games.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import { faCalendarDays, faCrown, faMedal } from '@fortawesome/free-solid-svg-icons';
 import { GamePointsPlayer, GamePointsScores } from "app/@shared/interface/game-points-scores";
@@ -14,27 +14,26 @@ import { CarouselModule } from "primeng/carousel";
   templateUrl: './point-games.component.html',
   styleUrl: './point-games.component.css'
 })
-export class PointGamesComponent {
+export class PointGamesComponent implements OnInit {
   @Input({required: true}) scores!: GamePointsScores[];
   faMedal = faMedal;
   faDate = faCalendarDays;
-  responsiveOptions: any[] | undefined = [
-    {
-      breakpoint: '1850px',
-      numVisible: 3,
-      numScroll: 1,
-    },
-    {
-      breakpoint: '1100px',
-      numVisible: 2,
-      numScroll: 1,
-    },
-    {
-      breakpoint: '750px',
-      numVisible: 1,
-      numScroll: 1,
-    }
-];
+  responsiveOptions: any[] | undefined;
+
+  ngOnInit(): void {
+    this.responsiveOptions = [
+      {
+        breakpoint: '2500px',
+        numVisible: 2,
+        numScroll: 1,
+      },
+      {
+        breakpoint: '1300px',
+        numVisible: 1,
+        numScroll: 1,
+      }
+    ];
+  }
 
 
   getColor(player: GamePointsPlayer): string {
